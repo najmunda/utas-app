a513435c9c4d024ab0e2c2ef71b299c52f2d0c17
Najmunda <najmundaakmal@gmail.com>
2025-05-31 06:30:55 +0700

    refactor: Add error to test master branch protection

--- src/states/users/reducer.js
+++ src/states/users/reducer.js
@@ -3,7 +3,7 @@ import { ActionType } from './action';
 export default function usersReducer(users = [], action = {}) {
   switch (action.type) {
     case ActionType.RECEIVE_USERS:
-      return action.payload.users;
+      return action.payload.user; // Intended Error Here
     default:
       return users;
   }
